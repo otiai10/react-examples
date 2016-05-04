a0212faa91d6255390ac383a40a5b0c56b7fc0f4
otiai10 <otiai10@gmail.com>
2016-05-04 10:21:45 +0200

    Foo not working

---
+++ foo/src/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Foo extends Component {
+  render() {
+    return (<h1>Foo</h1>)
+  }
+}
+
+export default Foo;
---
+++ foo/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './src',
+  output: {
+    filename:'./lib/index.js'
+  },
+  module: {
+    loaders: [
+      {test: /.jsx?$/, loader: 'babel-loader'}
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+}
